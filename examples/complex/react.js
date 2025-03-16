427c191cb81baf1f118c76cfd4545655cce502c8
Asko Nõmm <asko@nmm.ee>
2025-03-16 19:58:15 +0200

    Update example js

--- examples/complex/react.js
+++ examples/complex/react.js
@@ -2,7 +2,11 @@
 let react = await import("https://esm.sh/react@18.2.0");
 let reactDom = await import("https://esm.sh/react-dom@18.2.0");
 let exampleComponent = () => {
-return react.createElement("div", null, "Hello, React!");
+let [state, setState] = react.useState(0);
+react.useEffect(() => {
+console.log("This runs on mount.");
+}, []);
+return react.createElement("button", {"onClick": () => setState(state + 1)}, `Clicked  ${state}  times.`);
 };
 reactDom.render(react.createElement(exampleComponent), document.querySelector("#root"));
 })();
